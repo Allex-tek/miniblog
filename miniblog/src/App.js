47b2c5308acd5a25f3119c9d6c41e1b6983e3615
Allex-Lima <alexlmangabeira@gmail.com>
2025-07-15 12:55:23 -0300

    configutando o React Router

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Router, Route, Routes, Navigate  } from "react-router-dom";
+//pages
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
